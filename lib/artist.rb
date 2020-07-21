e33e8d1cbbf1d5ab966d64946ef2d62b6d320549
Maika Kailani <queenmab051@gmail.com>
2020-07-21 21:32:41 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -27,14 +27,9 @@ class Artist
     artist = @@all.find{|a| a.name == artist_name}
     return artist if artist
     artist = Artist.new(artist_name)
-
-  #   # @@all.find{|a| a.name == artist_name} || Artist.new(artist_name)
-
   end
 
   def print_songs
-    # @songs.each {|song| puts Song.artist.name}
-    # @@all.each {|song| puts song}
    self.songs.each {|song| puts song.name}
   end
 end
