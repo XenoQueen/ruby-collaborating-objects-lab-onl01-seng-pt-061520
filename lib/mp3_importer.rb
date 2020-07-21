f0dd1d73f13eaa909009b5aaf30f2583c24bfc50
Maika Kailani <queenmab051@gmail.com>
2020-07-21 21:32:10 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -9,14 +9,13 @@ class MP3Importer
 
   def files
     files = []
-    Dir.new(self.path).each do |file|
+    Direct.new(self.path).each do |file|
       files << file if file.length > 4
     end
     files
   end
 
   def import
-    # list_of_filename.each {|filename| Song.new_by_filename(filename)}
     self.files.each do |filename|
       Song.new_by_filename(filename)
     end
