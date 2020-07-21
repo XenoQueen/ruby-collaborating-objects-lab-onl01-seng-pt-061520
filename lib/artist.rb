e1a7f136263de19694054785faf70eca5276b3d0
Maika Kailani <queenmab051@gmail.com>
2020-07-21 21:31:15 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,35 +1,40 @@
 class Artist
-  
+
   attr_accessor :name, :songs
-  
+
   @@all = []
-  
+
   def initialize(name)
     @name = name
     @songs = []
     @@all << self
   end
-  
+
   def self.all
-    @@all
+    @@all  
   end
-  
-  def songs
-    Song.all.select {|song|song.artist == self}
-  end
-  
+
   def add_song(song)
     @songs << song
     song.artist = self
   end
-  
-  def self.find_or_create_by_name(artist_name)
-    artist = @@all.find{|a|a.name == artist_name}
-    #return artist if artist
-    artist = Artist.new(artist_name)
+
+  def songs
+  Song.all.select {|song| song.artist == self}
   end
-  
+
+  def self.find_or_create_by_name(artist_name)
+    artist = @@all.find{|a| a.name == artist_name}
+    return artist if artist
+    artist = Artist.new(artist_name)
+
+  #   # @@all.find{|a| a.name == artist_name} || Artist.new(artist_name)
+
+  end
+
   def print_songs
-    self.songs.each{|song| puts song.name}
+    # @songs.each {|song| puts Song.artist.name}
+    # @@all.each {|song| puts song}
+   self.songs.each {|song| puts song.name}
   end
 end
