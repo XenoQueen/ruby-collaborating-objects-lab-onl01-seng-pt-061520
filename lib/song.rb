ff8e12260af291e306f6dbd7170b372ed698a2bb
Maika Kailani <queenmab051@gmail.com>
2020-07-21 21:27:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,5 +14,14 @@ class Song
   end
   
   def new_by_filename(file)
-    
+    song_info = file.chomp(".mp3").split(" - ")
+    song = Song.new(song_info[1])
+    song.artist_name = song_info[0]
+    song
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end
 end
